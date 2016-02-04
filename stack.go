07f1d9f9eecd0bbf19fb8f01a075dcd8c6cd7082
sschellhoff <soeren.schellhoff@gmail.com>
2016-02-04 20:58:53 +0100

    stack implementation

--- stack.go
+++ stack.go
@@ -1,4 +1,34 @@
-package datastructure
+package gotastructures
 
-type stack struct {
+type Stack struct {
+	top *StackNode
+}
+
+type StackNode struct {
+	data interface{}
+	next *StackNode
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.top == nil
+}
+
+func (s *Stack) Peek() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	return s.top.data
+}
+
+func (s *Stack) Push(data interface{}) {
+	s.top = &StackNode{data, s.top}
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	data := s.top.data
+	s.top = s.top.next
+	return data
 }
